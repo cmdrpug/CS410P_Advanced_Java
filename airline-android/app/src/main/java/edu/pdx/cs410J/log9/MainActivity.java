94a46a73a3896365e865366b0c341ff5c18791ce
Logan Anderson <log9@pdx.edu>
2023-03-21 14:01:26 -0700

    Initial commit for project 6

---
+++ airline-android/app/src/main/java/edu/pdx/cs410J/log9/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.pdx.cs410J.log9;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
